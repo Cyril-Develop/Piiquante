9c8ab36a10d9eb9997d974bded313f3f0660c1b3
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2024-02-08 11:25:51 +0100

    modif carte

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -3,10 +3,14 @@ import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
 import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Card({ content }) {
 
-    console.log(content);
+    const { currentUser } = useContext(AuthContext);
+
+    console.log(content.mainPepper);
 
     return (
         <article className='card'>
@@ -16,25 +20,35 @@ export default function Card({ content }) {
             </div>
             <div className='card_content'>
                 <div className='card_content_infos'>
-                    <h2>
-                        <span>{content.name}</span>
-                        <span>by {content.manufacturer}</span>
-                    </h2>
-                    <p>{content.description}</p>
-                    <p>Nombre de likes : {content.likes}</p>
+                    <div className='card_content_infos_title'>
+                        <h2>
+                            <span>-{content.name}-</span>
+                            <span>by {content.manufacturer}</span>
+                        </h2>
+                        {content.mainPepper.length > 1 ?
+                            <p>{content.mainPepper.map((pepper, index) => <span key={index}>{pepper} </span>)}</p>
+                            :
+                            <p>{content.mainPepper[0]}</p>
+                        }
+                    </div>
+                    <p className='card_content_infos_description'>{content.description}</p>
                     <div className='card_content_infos_like'>
                         <button>
                             <ThumbUpOffAltIcon />
+                            <span>{content.usersLiked.length}</span>
                         </button>
                         <button>
                             <ThumbDownOffAltIcon />
+                            <span>{content.usersDisliked.length}</span>
                         </button>
                     </div>
                 </div>
-                <div className='card_content_actions'>
-                    <button>Update</button>
-                    <button>Delete</button>
-                </div>
+                {content.userId === currentUser.userId && 
+                    <div className='card_content_actions'>
+                        <button>Modifier</button>
+                        <button>Supprimer</button>
+                    </div>
+                }
             </div>
         </article>
     )
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext();
 
@@ -7,18 +7,18 @@ export const AuthContextProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(
         JSON.parse(localStorage.getItem("user")) || null
     );
- 
+
     useEffect(() => {
         localStorage.setItem("user", JSON.stringify(currentUser));
     }, [currentUser]);
-    
+
     const logout = () => {
         localStorage.removeItem("user");
         setCurrentUser(null);
     };
 
     return (
-        <AuthContext.Provider value={{  logout, setCurrentUser, currentUser }}>
+        <AuthContext.Provider value={{ logout, setCurrentUser, currentUser }}>
             {children}
         </AuthContext.Provider>
     )
