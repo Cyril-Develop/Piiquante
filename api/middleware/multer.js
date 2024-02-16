bcfca89ecddfa3617fb043fc108f2b21b501d98f
Cyril bourdon <cyril.bourdon@hotmail.fr>
2024-02-16 18:35:42 +0100

    modif file name + webp

--- api/middleware/multer.js
+++ api/middleware/multer.js
@@ -1,23 +1,11 @@
 const multer = require('multer');
 
-//Mime type dictionnary
-const MIME_TYPE = {
-    "image/jpg" : "jpg",
-    "image/jpeg" : "jpg",
-    "image/png" : "png",
-    "image/webp" : "webp"
-};
-
 const storage = multer.diskStorage({
     filename: (req, file, callback) => {
-        //Remove the spaces from the original file and replace them with _
-        const name = file.originalname.split(' ').join('_');
-        //browsers use the MIME type to determine how they will process or display the document
-        const extension = MIME_TYPE[file.mimetype];
-        //Using Date.now() and extension makes the filename unique
-        callback(null, name + Date.now() + '.' + extension)
+        const name = file.originalname.split('.')[0];
+        callback(null, name + Date.now())
     }
-})
+});
 
 //Export storage function and define how many file can be send
 module.exports = multer({storage}).single("image");
--- api/middleware/sharp.js
+++ api/middleware/sharp.js
@@ -3,13 +3,13 @@ const sharp = require('sharp');
 const resize = (req, res, next) => {
     if (req.file) {
         sharp(req.file.path)
+            .webp()
             .resize(450, 450)
-            .toFile(`images/${req.file.filename}`, (err, info) => {
+            .toFile(`images/${req.file.filename}.webp`, (err, info) => {
                 if (err) {
                     console.log('Error resizing image:', err);
                     return res.status(400).json({ error: 'Image not resized.' });
                 }
-                console.log('Image resized successfully:', info);
                 next();
             });
     } else {
